d0084c40b2a7221a47a9f39f68dd83f4d9d91ce7
sbassmed <sebastiancamilomarin@gmail.com>
2019-02-07 01:58:52 -0500

    Defino las rutas de recurso , configuro index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/show'
-  get 'posts/_form'
+
+  root 'posts#index'
+
+  resources :posts
+  resources :comments
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
