ab1667844d183e14d7fbb2714bab60b44b7f1171
sbassmed <sebastiancamilomarin@gmail.com>
2019-02-07 03:31:26 -0500

    Tratando de definir algunas acciones , testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,43 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def show
+      @comment = Comment.new
   end
 
   def edit
   end
 
-  def show
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post modificado exitosamente!"
+      redirect_to posts_path
+    else
+      flash[:danger] = "No se pudo modificar el post"
+      render :edit
+    end
   end
 
-  def _form
-  end
+  def destroy
+      if @post.destroy
+        flash[:success] = "Post eliminado exitosamente!"
+        redirect_to posts_path
+      else
+        flash[:danger] = "No se pudo eliminar el post"
+        render :index
+      end
+    end
+
+  private
+
+      def post_params
+        params.require(:post).permit(:title, :body, :user_id)
+      end
 end
