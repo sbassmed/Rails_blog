c82fff53e735b474d9a1930d7969e1109f69f028
sbassmed <sebastiancamilomarin@gmail.com>
2019-02-07 22:54:06 -0500

    Probar comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!
+  
+  def create
+    post = Post.find(params[:post_id])
+    post.comments.create(comments_params)
+      redirect_to post , notice: "El comentario fue publicado con éxito"
+  end
+
+  private
+		def comment_params
+			params.require(:comment).permit(:body, :post_id, :user_id)
+		end
 end
